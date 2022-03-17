d0a99892a874cde16d7e3601e89c0d17f9ecb36e
NickRichards13 <89357974+NickRichards13@users.noreply.github.com>
2022-03-17 12:57:14 +0300

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -6,15 +6,17 @@ using namespace std;
 int main() {
 	float x_center, y_center, r, x_point, y_point, x, y, sq_xy, sq_r;
 	int i = 0;
-	string line;
-
+	string file_name, line;
+    	cout << "Hello! Please, enter file name along with format (e.g. - test.txt): ";
+    	cin >> file_name;
+	
 	ifstream my_file1;
-	my_file1.open("file1.txt");
+	my_file1.open(file_name);
 	my_file1 >> x_center >> y_center >> r;
 	my_file1.close();
 
 	ifstream my_file2;
-	my_file2.open("file2.txt");
+	my_file2.open(file_name);
 	while (my_file2 >> x_point) {
 		i++;
 		my_file2 >> y_point;
